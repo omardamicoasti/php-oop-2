7d26fcc06b59971ff73084ade53846a66c5ebc51
Omar D'Amico <omar.damico.asti@gmail.com>
2021-05-11 16:42:03 +0200

    Primo push

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+class User {
+    private $username;
+    private $password;
+    private $email;
+    public $date_creation;
+    public $profile_pic = "default_user_logo.png";
+    public $name;
+    public $surname;
+    public $bio;
+    public $date_of_birth;
+
+    function __construct($email, $username, $password)
+    {
+        $this->email = $email;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function setDateCreation() {
+        date_default_timezone_set('Europe/Rome');
+        $this->date_creation = date('m/d/Y h:i:s a', time());
+    }
+    public function getDateCreation() {
+        return $this->date_creation;
+    }
+}
+
+$omardamico = new User("omar@email.it", "omardamicoasti", "SecretPassword111");
+$omardamico->setDateCreation();
+$omardamico->profile_pic = "best_profile_pic_omar.png";
+$omardamico->name = "omar";
+$omardamico->surname = "d'amico";
+$omardamico->bio = "hello everyone!";
+$omardamico->date_of_birth = "07/08/1989";
+
+var_dump($omardamico);
+
+$paolodossetto = new User("paolo@email.it", "paolodossetto", "SecretPassword222");
+$paolodossetto->setDateCreation();
+$paolodossetto->profile_pic = "best_profile_pic_paolo.png";
+$paolodossetto->name = "paolo";
+$paolodossetto->surname = "dossetto";
+$paolodossetto->bio = "hey everyone!";
+$paolodossetto->date_of_birth = "06/05/1993";
+
+var_dump($paolodossetto);
